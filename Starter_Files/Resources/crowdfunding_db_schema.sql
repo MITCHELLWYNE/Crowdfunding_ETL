6fd4d066b969322062b00d94c6a5dff70f14ecec
ailirrrrrrrr <122580261+ailirrrrrrrr@users.noreply.github.com>
2023-05-01 21:19:24 -0700

    Add files via upload
    crowsfunding_db

---
+++ Starter_Files/Resources/crowdfunding_db_schema.sql
@@ -0,0 +1,43 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/LfLYKe
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT NOT NULL,
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    "company_name" VARCHAR(100)   NOT NULL,
+    "description" VARCHAR(100)   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR(100)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(100)   NOT NULL,
+    "currency" VARCHAR(100)   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(100)  NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    "subcategory_id" VARCHAR(100)   NOT NULL,
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(100) PRIMARY KEY  NOT NULL,
+    "category" VARCHAR(100)   NOT NULL
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT PRIMARY KEY  NOT NULL,
+    "first_name" VARCHAR(100)   NOT NULL,
+    "last_name" VARCHAR(100)   NOT NULL,
+    "email" VARCHAR(100)   NOT NULL
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(100) PRIMARY KEY  NOT NULL,
+    "subcategory" VARCHAR(100)   NOT NULL
+);
+
+
